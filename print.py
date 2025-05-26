97034764e19f30887f0b7a177a6c9280f1a8985b
baddie113 <fjnintest@gmail.com>
2025-05-26 19:01:35 +0200

    Add files via upload

---
+++ print.py
@@ -0,0 +1,8 @@
+#print a welcome message
+print("welcome to python")
+#printing mt name
+print ("my name is nidhi")
+#printing fun fact
+print ("i love science")
+#perform a simple question in math
+print (1+2,2+1)
