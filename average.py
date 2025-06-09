cfeb624def7889ec26487d235eac85d249aa31df
baddie113 <fjnintest@gmail.com>
2025-06-09 18:51:24 +0200

    Add files via upload

---
+++ average.py
@@ -0,0 +1,9 @@
+Sub1 = 98
+Sub2 = 94
+Sub3 = 78
+Sub4 = 89
+Sub5 = 64
+Sum = Sub1 + Sub2 + Sub3 + Sub4 + Sub5
+print("The sum of the sub is: " , Sum)
+Average = Sum/5
+print("Te average of all the 5 Sub is: " , Average)
