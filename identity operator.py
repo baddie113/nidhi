862c7b07e23c3eb32e1a04255be4878c1bf095af
baddie113 <fjnintest@gmail.com>
2025-06-26 18:51:22 +0200

    Add files via upload

---
+++ identity operator.py
@@ -0,0 +1,16 @@
+x = 5
+if (type(x)is int):
+    print("x is an integer")
+else:
+    print("false")
+x = 5.9
+if (type(x) is not int):
+    print("x is not an integer")
+else:
+    print("false")
+x = 20
+y = 20
+if (x is y):
+    print("x & y are SAME identity")
+else:
+    print("x & y are SAME identity")
